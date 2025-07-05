ed9c985de276e8058870c0788082cf562a64085c
Nicolas Sepulveda <ni.sepulvedaa@gmail.com>
2025-07-05 12:33:47 -0400

    fix del fix archivo 06

--- 06-insert-fn-load-to-parquet-to-bq.sql
+++ 06-insert-fn-load-to-parquet-to-bq.sql
@@ -9,7 +9,7 @@ INSERT INTO `dev_config_zone.process_params` (
     'fn-load-to-parquet-to-bq',
     JSON '''
     [
-        {"input": "{{buckets_detino}}{{path_destino}}{{nombre_archivo}}","output": "{{proyecto}}.{{dataset_raw_zone}}.{{nombre_tabla_raw}}",  "periodicidad": "{{periodicidad}}"}
+        {"input": "{{buckets_detino}}{{path_destino}}/{{nombre_archivo}}","output": "{{proyecto}}.{{dataset_raw_zone}}.{{nombre_tabla_raw}}",  "periodicidad": "{{periodicidad}}"}
     ]
     ''',
     'workflow-arquetipo-ingesta-raw-{{version_arquetipo_raw}}',
