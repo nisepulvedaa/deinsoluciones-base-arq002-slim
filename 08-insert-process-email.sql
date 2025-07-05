c5edcc27e67a5c2694084e788e7ffdc7cf08445c
Nicolas Sepulveda <ni.sepulvedaa@gmail.com>
2025-07-05 10:02:43 -0400

    fix archivo 08

--- 08-insert-process-email.sql
+++ 08-insert-process-email.sql
@@ -11,8 +11,8 @@ INSERT INTO `dev_config_zone.process_email` (
         "email_to_0": "{{correos_destinatarios}}",
         "email_subj_err": "Resultado Ejecución Proceso: process-{{nombre_proceso}}-raw-{{version_arquetipo_raw}} [ERROR]", 
         "email_subj_ok": "Resultado Ejecución Proceso: process-{{nombre_proceso}}-raw-{{version_arquetipo_raw}} [OK]", 
-        "email_body_err": "Proces : process-{{nombre_proceso}}-raw de Arquetipo: workflow-arquetipo-ingesta-raw-{{version_arquetipo_raw}} Ejecutado Con Errores!", 
-        "email_body_ok": "Proces : process-{{nombre_proceso}}-raw de Arquetipo: workflow-arquetipo-ingesta-raw-{{version_arquetipo_raw}} Ejecutado Ok!"
+        "email_body_err": "Process : process-{{nombre_proceso}}-raw de Arquetipo: workflow-arquetipo-ingesta-raw-{{version_arquetipo_raw}} Ejecutado Con Errores!", 
+        "email_body_ok": "Process : process-{{nombre_proceso}}-raw de Arquetipo: workflow-arquetipo-ingesta-raw-{{version_arquetipo_raw}} Ejecutado Ok!"
         }
     ]
     ''',
